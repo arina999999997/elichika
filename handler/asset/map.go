bf349a86e86c0a416f0c95e63e713e0e891b6f3e
Arina Aria <arina999999997@gmail.com>
2026-05-29 22:05:21 +0700

    Enforce stronger requirement for CDN related endpoints

--- handler/asset/map.go
+++ handler/asset/map.go
@@ -2,6 +2,7 @@ package asset
 
 import (
 	"elichika/assetdata"
+	"elichika/config"
 	"elichika/log"
 	"elichika/router"
 
@@ -12,6 +13,9 @@ import (
 
 // acting as the cdn, we need a map from file to actual files
 func staticMap(ctx *gin.Context) {
+	if (*config.Conf.CdnServer != "elichika") && (*config.Conf.CdnServer != "elichika_tls") {
+		log.Panic("staticMap is not allowed because CDN is not elichika or elichika tls")
+	}
 	file := ctx.Param("fileName")
 	downloadData := assetdata.GetDownloadData(file)
 	if downloadData.IsEntireFile {
